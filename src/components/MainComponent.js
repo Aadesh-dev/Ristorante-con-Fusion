9dd60216e59579cf705b3147723bdd0db69aa7ac
Aadesh Puthran <puthranaadesh3@gmail.com>
2021-03-15 16:34:50 +0530

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -9,6 +9,7 @@ import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import Contact from "./ContactComponent";
+import About from "./AboutComponent";
 import { Route, Switch, Redirect } from "react-router-dom";
 
 class Main extends Component {
@@ -71,6 +72,10 @@ class Main extends Component {
           />
           <Route path="/menu/:dishId" component={DishWithId} />
           <Route path="/contactus" component={Contact} />
+          <Route
+            path="/aboutus"
+            component={() => <About leaders={this.state.leaders} />}
+          />
           <Redirect to="/home" />
         </Switch>
 
